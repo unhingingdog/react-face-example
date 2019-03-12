70748dd54c33989fa1ae287cf54c8af3abd5301f
HAMISH <constatcopywriting@gmail.com>
2019-03-13 10:50:23 +1300

    Created spinner icon to test react-face.

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FaceDetector from 'react-face'
+
+class Spinner extends Component {
+  state = { 
+    deg: 0,
+    acc: 0
+  }
+
+  componentDidUpdate() {
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        this.setState({
+          deg: this.state.deg + this.state.acc,
+          acc: ((50 - this.props.x) * -1) / 100
+        })
+      }, 100)
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <img 
+          src={logo} 
+          className="App-logo" 
+          alt="logo" 
+          style={{
+            transform: `
+              rotate(${this.state.deg}deg) 
+            `,
+            transition: 'transform 0.1s linear'
+          }}
+          
+        />
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">  
+          <FaceDetector>
+            {data => <Spinner x={data[0].x} />}
+          </FaceDetector>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
